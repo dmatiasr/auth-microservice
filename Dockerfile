01f2afe9767a5ef5fa8886a39a1d5f293b486078
mrondeau <matias.rondeau@onapsis.com>
2021-05-29 16:55:34 -0300

    PoC:
    Adding endpoints to API.
    Check performance

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,9 @@ COPY ./authenticator /opt/authenticator
 WORKDIR /opt/authenticator
 
 
-RUN apt install -y python3-pip
-RUN pip install fastapi uvicorn
+RUN apt install -y build-essential libssl-dev libffi-dev python-dev
+RUN apt install -y python3-pip 
+RUN pip install -r requirements.txt
 
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
--- authenticator/main.py
+++ authenticator/main.py
@@ -1,8 +1,15 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from .auth import AuthHandler
+from .schemas import AuthDetails
+
 
 app = FastAPI()
+
+
+auth_handler = AuthHandler()
+users = []
 
 
 @app.get("/")
@@ -13,3 +20,40 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post('/register', status_code=201)
+def register(auth_details: AuthDetails):
+    if any( x.get('username') == auth_details.username for x in users):
+        raise HTTPException(status_code=400, detail="Username already exist")
+    hashed_password = auth_handler.get_password_hash(auth_details.password)
+    users.append({
+        "username": auth_details.username,
+        "password": hashed_password
+    })
+
+    return 200
+
+
+@app.post('/login')
+def login(auth_details: AuthDetails):
+    user = None
+    for x in users:
+        if x.get("username") == auth_details.username:
+            user = x 
+            break
+
+    if (user is None) or (not auth_handler.verify_password(auth_details.password, user.get("password")))
+        raise HTTPException(status_code=401, detail="Invalid username nor password")
+    token = auth_handler.encode_token(user.get("username"))
+    
+    return {"token": token}
+
+
+@app.get('/free')
+def free():
+    return {"Hello": "World"}
+
+@app.get('/private')
+def private(username=Depends(auth_handler.auth_wrapper)):
+    return {"name": username}
